194594605bc62f0154342997519115a4e2b67815
gertjan vanhove <gertjan_vanhove@hotmail.com>
2013-05-08 12:42:37 +0200

    startscherm uitgebouwd

---
+++ classes/Connection.php
@@ -0,0 +1,7 @@
+<?php 
+		$sHost = "localhost";
+		$sUser = "root";
+		$sPassword = "";
+		$sDatabase = "project2_2013";
+		$link = @mysqli_connect($sHost, $sUser, $sPassword, $sDatabase) or die("Oop, dbase is gone!");	
+?>
